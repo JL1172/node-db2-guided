0a8bfaf0aefa0790c9b8f452e5b272892eb6af8c
Jacob Lang <jacoblang72@comcast.net>
2023-10-03 17:26:04 -0400

    migrated

--- data/db-config.js
+++ data/db-config.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 
 const configs = require("../knexfile");
 
-const accessKey = "development";
+const env = process.env.NODE_ENV || "development";
 
-module.exports = knex(configs[accessKey]); 
+module.exports = knex(configs[env]);  
---
+++ data/migrations/20231003212132_first-migration.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.createTable("fruits",table => {
+    table.increments("fruits_id")
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("fruits")
+};
