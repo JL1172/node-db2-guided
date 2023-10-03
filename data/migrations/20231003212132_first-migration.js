f71b5b3d4d6c5d21b4238e3d7fc0c338f1a914f5
Jacob Lang <jacoblang72@comcast.net>
2023-10-03 18:31:33 -0400

    done

--- data/migrations/20231003212132_first-migration.js
+++ data/migrations/20231003212132_first-migration.js
@@ -5,6 +5,9 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("fruits",table => {
     table.increments("fruits_id")
+    table.string('fruits_name',100).notNullable().unique();
+    table.decimal('avg_weight_oz').notNullable();
+    table.boolean("delicious")
   })
 };
 
